d7e89b78af574108c522c0740f0182d307b672b7
msullivan1999 <matt.r.sullivan1999@gmail.com>
2021-09-02 23:16:13 +1000

    Started styling using Bootstrap
    - coloured, centred

--- js/calc.js
+++ js/calc.js
@@ -13,18 +13,25 @@ buttons.forEach(function(button) {
 function calculate(event) {
   // current clicked buttons value
   const clickedButtonValue = event.target.value;
+  var flag = 0;
 
   switch(clickedButtonValue) {
     case '=':
         if (display.value !== '') {
             display.value = eval(display.value);
             }
+        flag = 1;
         break;
     case 'C':
         display.value = '';
+        flag = 0;
         break;
     default:
+      if(flag !== 1) {
         display.value += clickedButtonValue;
+      }
   }
 }
+
+// add more logic to improve functionality
 
